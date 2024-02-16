212c8749e9ba59f7e16bce7a6774f0ca34d0caa1
ilkeery <110738491+ilkeery@users.noreply.github.com>
2024-02-16 19:22:05 +0300

    += is better than the append func

--- leetcode/1.cpp
+++ leetcode/1.cpp
@@ -2,32 +2,33 @@ class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
         string result = "";
-        int len_1 = word1.length();
-        int len_2 = word2.length();
-        if(len_1>len_2)
+        int len_1 = word1.length(); // n
+        int len_2 = word2.length(); // n 
+        if(len_1>len_2) //1
         {
-            for(int i=0; i<len_2; i++)
+            for(int i=0; i<len_2; i++) //n
             {
-                result.append(1,word1[i]);
-                result.append(1,word2[i]);
+                result += word1[i]; // 1*n
+                result += word2[i]; // 1*n
+                
             }
-            for(int i=len_2; i<len_1; i++)
+            for(int i=len_2; i<len_1; i++) // n or 1?
             {
-                result.append(1, word1[i]);
+                result += word1[i]; // 1*n
             }
         }
         else
         {
-            for(int i=0; i<len_1; i++)
+            for(int i=0; i<len_1; i++) //n
             {
-                result.append(1,word1[i]);
-                result.append(1,word2[i]);
+                result += word1[i]; // 1*n
+                result += word2[i]; // 1*n
             }
-            for(int i=len_1; i<len_2; i++)
+            for(int i=len_1; i<len_2; i++) //n
             {
-                result.append(1, word2[i]);
+                result += word2[i]; // 1*n
             }
         }
-    return result;    
+    return result; // Time complexity: O(n), space complexity: O(n)
     }
 };
